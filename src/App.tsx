9c3efc0eb2bb1cbfd94bf963d4ea36934930670e
qubalee <qubalee@live.com>
2025-10-19 10:47:57 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <div className="flex flex-col min-h-screen">
           <Navigation />
           <main className="flex-1">
